218ea600e93b208f93e8990a63e6f3fd2c17463a
Michaela-Purkert <129502870+Michaela-Purkert@users.noreply.github.com>
2023-04-04 12:28:39 +0200

    Development (#1)
    * initial
    
    * Class Operations, MenuText, SimpleCalculator
    
    * Program
    
    ---------
    
    Co-authored-by: Chlupus <purkertovam@gmail.com>

---
+++ MenuText.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class MenuText
+    {
+        public void MenuOptions()
+        {
+            Console.WriteLine("Choose an option:");
+
+            Console.WriteLine("1 - Choose first number");
+            Console.WriteLine("2 - Choose second number");
+            Console.WriteLine("3 - Addition");
+            Console.WriteLine("4 - Subtraction");
+            Console.WriteLine("5 - Multiplication");
+            Console.WriteLine("6 - Division");
+            Console.WriteLine("7 - Show the last 3 results");
+            Console.WriteLine("8 - End of program");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Calculator;
+
+Console.WriteLine("Welcome to the simple calculator!\n");
+
+SimpleCalculator calculator = new SimpleCalculator();
+
+calculator.Menu();
